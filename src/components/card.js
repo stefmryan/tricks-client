7a3d0b3d1324c3b1e6a990dd959235d5c78926ec
stefmryan <stefmryan@gmail.com>
2022-09-27 12:10:18 -0400

    add images to card component:

--- src/components/card.js
+++ src/components/card.js
@@ -1,6 +1,16 @@
 import React from "react";
+import deckOfCard from "../utils/deckOfCards";
 
 const Card = () => {
-  return <div>Testing that this works</div>;
+  const randomIndex = Math.floor(Math.random() * deckOfCard.length);
+
+  return (
+    <div>
+      <img
+        src={`./images/cards/face/${deckOfCard[randomIndex].image}`}
+        alt='face'
+      />
+    </div>
+  );
 };
 export default Card;
