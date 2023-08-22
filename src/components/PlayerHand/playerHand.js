8a3f77d2adee52f2442ca8b00289e1c8b09cd686
stefmryan <stefmryan@gmail.com>
2023-08-22 14:15:01 -0400

    css for round header

--- src/components/PlayerHand/playerHand.js
+++ src/components/PlayerHand/playerHand.js
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import Card from "../Card/card";
 import styles from "../PlayerHand/playerHand.module.css";
 
-const PlayerHand = (/*{ round }*/ { name }) => {
-  //const [rounds] = useState(round);
+const PlayerHand = ({ name }) => {
   const [, setPlayerHand] = useState([]);
   return (
     <div className={styles.container}>
--- src/pages/Gamepage.js
+++ src/pages/Gamepage.js
@@ -6,16 +6,21 @@ import styles from "../pages/Gamepage.module.css";
 const GamePage = () => {
   const location = useLocation();
   let [players] = useState([location.state.players]);
+  const [round /*setRound*/] = useState(0);
 
   return (
-    <div className={styles.container}>
-      <Table
-        playerObjs={
-          location.state.players[0] === 1
-            ? (players[0] = ["player", "opponent1", "opponent2", "opponent3"])
-            : (players[0] = location.state.players)
-        }
-      />
+    <div>
+      <div className={styles.center}>Round {round}</div>
+      <div className={styles.grid_container}>
+        <div>placeholder for table</div>
+        <Table
+          playerObjs={
+            location.state.players[0] === 1
+              ? (players[0] = ["player", "opponent1", "opponent2", "opponent3"])
+              : (players[0] = location.state.players)
+          }
+        />
+      </div>
     </div>
   );
 };
